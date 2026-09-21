03a1901c9bd6d9ac871bd8ef9c938f7de80090c4
wputr <2582003@unai.edu>
2026-09-21 10:45:57 +0700

    commit 1: tambah input nilai dan konstanta bobot

--- src/NilaiAkhir.java
+++ src/NilaiAkhir.java
@@ -23,20 +23,5 @@ public class NilaiAkhir {
 
         System.out.print("Nilai final     : ");
         double finalNilai = scanner.nextDouble();
-        // Perhitungan tidak membutuhkan kurung karena operator * dikerjakan lebih dahulu daripada +.
-        double akhir = praktikum * BOBOT_PRAKTIKUM
-                + tugas * BOBOT_TUGAS
-                + mid * BOBOT_MID;
-
-        // Augmented assignment digunakan untuk menambahkan nilai final.
-        akhir += finalNilai * BOBOT_FINAL;
-
-        // Jika menggunakan int, hasilnya 78 karena pembagian integer membuang bagian desimal.
-        // Contoh: praktikum * 30 / 100 + tugas * 20 / 100 + mid * 20 / 100 + finalNilai * 30 / 100.
-
-        // Pembulatan dilakukan secara eksplisit menggunakan casting ke int.
-        int dibulatkan = (int) akhir;
-
-        // Menghitung selisih antara nilai akhir dan nilai yang sudah dibulatkan.
-        double selisih = akhir - dibulatkan;
-
+    }
+}
